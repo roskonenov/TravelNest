9ce16fb9c0c0a8d3a2f367c20b6cca23448fc8f6
Rosen <roskonenov@gmail.com>
2024-06-16 23:50:41 +0300

    add services controller, wip on index and services html

---
+++ src/main/java/bg/softuni/travelNest/web/ServicesController.java
@@ -0,0 +1,13 @@
+package bg.softuni.travelNest.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ServicesController {
+
+    @GetMapping("/services")
+    public String getServices(){
+        return "services";
+    }
+}
