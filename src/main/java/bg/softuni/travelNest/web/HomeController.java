21ef2eae7c951e89a3744513ccffcd1fd7221e84
Rosen <roskonenov@gmail.com>
2024-06-29 17:37:12 +0300

    add login and register template, also changes here and there. A wip.

--- src/main/java/bg/softuni/travelNest/web/HomeController.java
+++ src/main/java/bg/softuni/travelNest/web/HomeController.java
@@ -6,8 +6,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    @GetMapping("/services")
+    public String getServices(){
+        return "services";
+    }
+
+    @GetMapping("/projects")
+    public String getProjects(){
+        return "projects";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
+
     @GetMapping("/")
     public String showHome(){
         return "index";
+    }
+
+    @GetMapping("/testimonial")
+    public String testimonial(){
+        return "testimonial";
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @GetMapping("/register")
+    public String register(){
+        return "register";
+    }
+
+    @GetMapping("/about")
+    public String showAbout(){
+        return "about";
     }
 }
